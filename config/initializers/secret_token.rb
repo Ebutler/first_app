d78fa41f11814e64fca96d0a8173563828c8ee0d
Ed Butler <elb-msb@gametrail.org>
2015-06-17 21:02:39 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '427716bb22efdf5e0494d630adfa1a5981b29a6733c72fdc57ee97e3b38e60a0a674d1cfef97b98f30a1e187202dc06ae66463daea631bb3bbfd9f7258987dde'
